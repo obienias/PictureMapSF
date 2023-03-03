1f5068e16f9047bfcf2db77dc55be489bb08ff54
obienias <olga.julia.bienias@gmail.com>
2023-03-03 09:43:42 -0800

    updated search function

--- flickr-search.py
+++ flickr-search.py
@@ -16,8 +16,8 @@ search_query = {
     'lat': '37.773972',
     'lon': '-122.4194',
     'radius': '10',
-    'min_taken_date': '20223-01-01',
-    'max_taken_date': '2023-02-01',
+    'min_taken_date': '20223-03-02',
+    'max_taken_date': '2023-03-02',
     'page': 1,
 }
 
@@ -32,65 +32,70 @@ results_first = flickr.photos.search(**search_query)
 #gets total number of pages returned by API
 num_pages_total = results_first['photos']['pages']
 
-number = 0
+results = []
+
+#checks if the number of pages == 0 or more 
+if num_pages_total == 1:
+    results.ppend(results_first['photos']['photo'])
+if num_pages_total > 1:
+    #loop over paginated API
+    for page_current in range(1, num_pages_total + 1):
+        search_query['page'] = page_current  # set the current page number
+        response = flickr.photos.search(**search_query)  # make the API request
+        results.append(response['photos']['photo'])
+
+print (results)
+print (num_pages_total)
 
 #sets new dictionary and list to contain photo info
 photo_dict = {}
 photo_list = []
 
-results = []
-
-#loop over paginated API
-for page_current in range(1, total_pages + 1):
-    search_query['page'] = page_current  # set the current page number
-    response = flickr.photos.search(**search_query)  # make the API request
-    resultsa.append(response['photos']['photo'])
-
 #loops over results of search query
+number = 0
 
-for photo in results['photos']['photo']:
+# for photo in results['photos']['photo']:
 
-    photo_info = flickr.photos.getInfo(photo_id=photo['id'], secret=photo['secret'])
+#     photo_info = flickr.photos.getInfo(photo_id=photo['id'], secret=photo['secret'])
 
-    #retrieve parameters using get method
-
-    photo_id = int(photo.get('id'))
-    title = photo.get('title')
-    latitude = float(photo_info['photo']['location'].get('latitude'))
-    longitude = float(photo_info['photo']['location'].get('longitude'))
-    time_taken = photo_info['photo']['dates'].get('taken')
-    hour_taken = int(time_taken.split()[-1].split(':')[0])
-    photo_url = f"https://farm{photo.get('farm')}.staticflickr.com/{photo.get('server')}/{photo.get('id')}_{photo.get('secret')}.jpg"
-    author_id = photo.get('owner')
-    author_name = photo_info['photo']['owner'].get('username')
-    # author_profile_url = 1
-    # tags = photo_info['photo'].get('tags')``
-
-
-     # Add photo information to dictionary
-    photo_dict = {
-        'photo_id': photo_id,
-        'title': title,
-        'latitude': latitude,
-        'longitude': longitude,
-        'time_taken': time_taken,
-        'hour_taken': hour_taken,
-        'photo_url': photo_url,
-        'author_id': author_id,
-        'author_name': author_name
-    }
-
-    photo_list.append(photo_dict)
-
-    number += 1
-    # print (number)
-    # print(photo_info)
-    # print (photo)
+#     #retrieve parameters using get method
+#     photo_id = int(photo.get('id'))
+#     title = photo.get('title')
+#     latitude = float(photo_info['photo']['location'].get('latitude'))
+#     longitude = float(photo_info['photo']['location'].get('longitude'))
+#     time_taken = photo_info['photo']['dates'].get('taken')
+#     hour_taken = int(time_taken.split()[-1].split(':')[0])
+#     photo_url = f"https://farm{photo.get('farm')}.staticflickr.com/{photo.get('server')}/{photo.get('id')}_{photo.get('secret')}.jpg"
+#     author_id = photo.get('owner')
+#     author_name = photo_info['photo']['owner'].get('username')
+#     # author_profile_url = 1
+#     # tags = photo_info['photo'].get('tags')``
 
 
-print (number)
-# print (photos_dict)
+#      # Add photo information to dictionary
+#     photo_dict = {
+#         'photo_id': photo_id,
+#         'title': title,
+#         'latitude': latitude,
+#         'longitude': longitude,
+#         'time_taken': time_taken,
+#         'hour_taken': hour_taken,
+#         'photo_url': photo_url,
+#         'author_id': author_id,
+#         'author_name': author_name
+#     }
 
-with open("data/photos.json", 'w') as f:
-    json.dump(photo_list, f)
+#     photo_list.append(photo_dict)
+
+#     number += 1
+#     # print (number)
+#     # print(photo_info)
+#     # print (photo)
+
+
+# print (number)
+# # print (photos_dict)
+
+# with open("data/photos.json", 'w') as f:
+#     json.dump(photo_list, f)
 
